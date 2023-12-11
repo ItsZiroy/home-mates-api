88b95ccdcd428659713ad5011aca510c6b94c654
Yannik Hahn <yannik@h4hn.de>
2023-12-11 16:23:04 +0100

    adds user group route

--- api.ts
+++ api.ts
@@ -286,6 +286,12 @@ export interface Group {
      * @memberof Group
      */
     'shoppingList'?: ShoppingList;
+    /**
+     * 
+     * @type {Set<User>}
+     * @memberof Group
+     */
+    'users'?: Set<User>;
 }
 /**
  * 
@@ -1652,6 +1658,43 @@ export const GroupApiAxiosParamCreator = function (configuration?: Configuration
                 options: localVarRequestOptions,
             };
         },
+        /**
+         * 
+         * @param {string} id 
+         * @param {*} [options] Override http request option.
+         * @throws {RequiredError}
+         */
+        getUsers: async (id: string, options: AxiosRequestConfig = {}): Promise<RequestArgs> => {
+            // verify required parameter 'id' is not null or undefined
+            assertParamExists('getUsers', 'id', id)
+            const localVarPath = `/groups/{id}/users`
+                .replace(`{${"id"}}`, encodeURIComponent(String(id)));
+            // use dummy base URL string because the URL constructor only accepts absolute URLs.
+            const localVarUrlObj = new URL(localVarPath, DUMMY_BASE_URL);
+            let baseOptions;
+            if (configuration) {
+                baseOptions = configuration.baseOptions;
+            }
+
+            const localVarRequestOptions = { method: 'GET', ...baseOptions, ...options};
+            const localVarHeaderParameter = {} as any;
+            const localVarQueryParameter = {} as any;
+
+            // authentication jwt required
+            // http bearer authentication required
+            await setBearerAuthToObject(localVarHeaderParameter, configuration)
+
+
+    
+            setSearchParams(localVarUrlObj, localVarQueryParameter);
+            let headersFromBaseOptions = baseOptions && baseOptions.headers ? baseOptions.headers : {};
+            localVarRequestOptions.headers = {...localVarHeaderParameter, ...headersFromBaseOptions, ...options.headers};
+
+            return {
+                url: toPathString(localVarUrlObj),
+                options: localVarRequestOptions,
+            };
+        },
     }
 };
 
@@ -1686,6 +1729,18 @@ export const GroupApiFp = function(configuration?: Configuration) {
             const operationBasePath = operationServerMap['GroupApi.getGroup']?.[index]?.url;
             return (axios, basePath) => createRequestFunction(localVarAxiosArgs, globalAxios, BASE_PATH, configuration)(axios, operationBasePath || basePath);
         },
+        /**
+         * 
+         * @param {string} id 
+         * @param {*} [options] Override http request option.
+         * @throws {RequiredError}
+         */
+        async getUsers(id: string, options?: AxiosRequestConfig): Promise<(axios?: AxiosInstance, basePath?: string) => AxiosPromise<Array<UserDto>>> {
+            const localVarAxiosArgs = await localVarAxiosParamCreator.getUsers(id, options);
+            const index = configuration?.serverIndex ?? 0;
+            const operationBasePath = operationServerMap['GroupApi.getUsers']?.[index]?.url;
+            return (axios, basePath) => createRequestFunction(localVarAxiosArgs, globalAxios, BASE_PATH, configuration)(axios, operationBasePath || basePath);
+        },
     }
 };
 
@@ -1713,6 +1768,15 @@ export const GroupApiFactory = function (configuration?: Configuration, basePath
          */
         getGroup(id: string, options?: any): AxiosPromise<Group> {
             return localVarFp.getGroup(id, options).then((request) => request(axios, basePath));
+        },
+        /**
+         * 
+         * @param {string} id 
+         * @param {*} [options] Override http request option.
+         * @throws {RequiredError}
+         */
+        getUsers(id: string, options?: any): AxiosPromise<Array<UserDto>> {
+            return localVarFp.getUsers(id, options).then((request) => request(axios, basePath));
         },
     };
 };
@@ -1744,6 +1808,17 @@ export class GroupApi extends BaseAPI {
      */
     public getGroup(id: string, options?: AxiosRequestConfig) {
         return GroupApiFp(this.configuration).getGroup(id, options).then((request) => request(this.axios, this.basePath));
+    }
+
+    /**
+     * 
+     * @param {string} id 
+     * @param {*} [options] Override http request option.
+     * @throws {RequiredError}
+     * @memberof GroupApi
+     */
+    public getUsers(id: string, options?: AxiosRequestConfig) {
+        return GroupApiFp(this.configuration).getUsers(id, options).then((request) => request(this.axios, this.basePath));
     }
 }
 
